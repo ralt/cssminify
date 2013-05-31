2ce59b9283d1491bbae8a05e9674c2a64ede3a09
Florian Margaine <florian@margaine.com>
2013-05-31 19:58:21 +0200

    Walking files and getting content is done.

--- Cssfile.go
+++ Cssfile.go
@@ -1,11 +1,37 @@
 package cssminify
 
+import (
+	"path/filepath"
+	"os"
+	"regexp"
+	"io/ioutil"
+)
+
 func Files() []Cssfile {
 	return findFiles()
 }
 
 func findFiles() []Cssfile {
-	return make([]Cssfile, 1)
+	files := make([]Cssfile, 0)
+	filepath.Walk(".",
+		func (root string, info os.FileInfo, err error) error {
+
+		matched, _ := regexp.MatchString(".css$", root)
+		if matched {
+			file := Cssfile{pathname: root, content: readFile(root)}
+			files = append(files, file)
+		}
+		return err
+	})
+	return files
+}
+
+func readFile(root string) string {
+	content, err := ioutil.ReadFile(root)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
 }
 
 type Cssfile struct {
