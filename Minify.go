594af9d0bfa574748c6412a87468e1e79d0ef7c4
Florian Margaine <florian@margaine.com>
2013-06-06 22:00:19 +0200

    hex minification done

--- Minify.go
+++ Minify.go
@@ -54,7 +54,31 @@ func minifyVal(value string) string {
 }
 
 func cleanHex(value string) string {
+	re := regexp.MustCompile(`#(\d{6})`)
+	matches := re.FindAllString(value, -1)
+	if matches != nil {
+		for _, hex := range matches {
+			if isFull(hex) {
+				r := strings.NewReplacer(hex, newHex(hex))
+				value = r.Replace(value)
+			}
+		}
+	}
 	return value
+}
+
+func isFull(hex string) bool {
+	cmp := []byte(hex)[1:]
+	for _, l := range cmp {
+		if cmp[0] != l {
+			return false
+		}
+	}
+	return true
+}
+
+func newHex(hex string) string {
+	return string([]byte(hex)[:4])
 }
 
 func cleanUrl(value string) string {
